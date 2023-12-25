dc8ac06f05137719e68314607b4b5876d534eb31
Brian Howell <brianhowell@pm.me>
2023-12-24 23:15:32 -0500

    CSS reset, more boilerplate adjustment

---
+++ src/component/Minesweeper/Minesweeper.js
@@ -0,0 +1,13 @@
+import "./Minesweeper.css";
+
+function App() {
+  return (
+    <div id="Minesweeper">
+      <header>This is the header</header>
+      <main>This is where gameplay occurs</main>
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
+
+export default App;
