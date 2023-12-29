a3ab4e882d771e6f7a70e38700318462dac26e9f
Brian Howell <brianhowell@pm.me>
2023-12-29 17:25:14 -0500

    Style radio buttons

--- src/components/DifficultyForm/DifficultyForm.js
+++ src/components/DifficultyForm/DifficultyForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { EASY, MEDIUM, HARD } from "../../helpers/Constants";
+import "./DifficultyForm.css";
 
 const Settings = ({ difficultyChange }) => {
   const [selected, setSelected] = useState(EASY);
@@ -19,37 +20,51 @@ const Settings = ({ difficultyChange }) => {
   };
 
   return (
-    <form>
-      <input
-        type="radio"
-        name="difficulty"
-        id={EASY}
-        value={EASY}
-        checked={selected === EASY}
-        onChange={onChange}
-        onClick={onClick}
-      />
-      <label htmlFor={EASY}>Easy</label>
-      <input
-        type="radio"
-        name="difficulty"
-        id={MEDIUM}
-        value={MEDIUM}
-        checked={selected === MEDIUM}
-        onChange={onChange}
-        onClick={onClick}
-      />
-      <label htmlFor={MEDIUM}>Medium</label>
-      <input
-        type="radio"
-        name="difficulty"
-        id={HARD}
-        value={HARD}
-        checked={selected === HARD}
-        onChange={onChange}
-        onClick={onClick}
-      />
-      <label htmlFor={HARD}>Hard</label>
+    <form id="difficultyForm">
+      <div className="radio-wrapper selected">
+        <label className={selected === EASY && "selected"} htmlFor={EASY}>
+          Easy
+        </label>
+        <input
+          type="radio"
+          name="difficulty"
+          id={EASY}
+          value={EASY}
+          checked={selected === EASY}
+          onChange={onChange}
+          onClick={onClick}
+        />
+      </div>
+
+      <div className="radio-wrapper">
+        <label className={selected === MEDIUM && "selected"} htmlFor={MEDIUM}>
+          Medium
+        </label>
+        <input
+          type="radio"
+          name="difficulty"
+          id={MEDIUM}
+          value={MEDIUM}
+          checked={selected === MEDIUM}
+          onChange={onChange}
+          onClick={onClick}
+        />
+      </div>
+
+      <div className="radio-wrapper">
+        <label className={selected === HARD && "selected"} htmlFor={HARD}>
+          Hard
+        </label>
+        <input
+          type="radio"
+          name="difficulty"
+          id={HARD}
+          value={HARD}
+          checked={selected === HARD}
+          onChange={onChange}
+          onClick={onClick}
+        />
+      </div>
     </form>
   );
 };
