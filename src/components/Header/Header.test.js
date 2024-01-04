6f3909a7d4b67417fc94110d4010a8f5d4a08b5d
Brian Howell <brianhowell@pm.me>
2024-01-04 17:38:41 -0500

    Remove unused import

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import DifficultyForm from "../DifficultyForm/DifficultyForm";
 import { render, screen } from "@testing-library/react";
 import { PLAYING } from "../../helpers/Constants";
 
