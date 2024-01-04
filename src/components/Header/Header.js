6fe724d82d255b5a73bba0ff449e70eeb4a2f0e4
Brian Howell <brianhowell@pm.me>
2024-01-04 17:37:43 -0500

    Header tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,3 +22,4 @@ const Header = ({ difficultyChange, gameState, bombsRemaining }) => {
 };
 
 export default Header;
+
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,3 +1,38 @@
-test("TODO", () => {
-  expect(1).toEqual(1);
+import React from "react";
+import Header from "./Header";
+import DifficultyForm from "../DifficultyForm/DifficultyForm";
+import { render, screen } from "@testing-library/react";
+import { PLAYING } from "../../helpers/Constants";
+
+const setup = (gameState = PLAYING) => {
+  render(
+    <Header
+      gameState={gameState}
+      bombsRemaining={10}
+      difficultyChange={() => {}}
+    />
+  );
+};
+
+describe("Header", () => {
+  test("should render the icon", () => {
+    setup();
+    const img = screen.getByAltText("Minesweeper");
+    expect(img).toBeVisible();
+    expect(img).toHaveAttribute("src", "header-logo.png");
+  });
+  test("should render the title", () => {
+    setup();
+    expect(screen.getByText("Minesweeper")).toBeVisible();
+  });
+  test("should render the DifficultyForm", () => {
+    setup();
+    expect(screen.getByText("Easy")).toBeVisible();
+    expect(screen.getByText("Medium")).toBeVisible();
+    expect(screen.getByText("Hard")).toBeVisible();
+  });
+  test("should render the Timer", () => {
+    setup();
+    expect(screen.getByText("0:00")).toBeVisible();
+  });
 });
