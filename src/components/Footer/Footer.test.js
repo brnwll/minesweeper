02e903c8a4671cd344962972bf62e5df8318c8b2
Brian Howell <brianhowell@pm.me>
2024-01-04 15:57:20 -0500

    Footer tests

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,3 +1,18 @@
-test("TODO", () => {
-  expect(1).toEqual(1);
+import React from "react";
+import Footer from "./Footer";
+import { render, screen } from "@testing-library/react";
+
+describe("Footer", () => {
+  test('should render "Created by" text', () => {
+    render(<Footer />);
+    expect(screen.getByText("Created by")).toBeVisible();
+  });
+
+  test("should contain a link to the GitHub repo", () => {
+    render(<Footer />);
+    expect(screen.getByText("brnwll")).toHaveAttribute(
+      "href",
+      "https://github.com/brnwll/minesweeper"
+    );
+  });
 });
